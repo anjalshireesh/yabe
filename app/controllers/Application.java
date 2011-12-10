ec3af28b9ce498575ea5492e753e68284c3572e3
Shireesh Anjal <anjalshireesh@gmail.com>
2011-12-10 21:41:47 +0530

    Viewing and posting comments

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.validation.*;
 
 import java.util.*;
 
@@ -17,10 +18,22 @@ public class Application extends Controller {
 
     public static void index() {
         Post frontPost = Post.find("order by postedAt desc").first();
-        List<Post> olderPosts = Post.find(
-            "order by postedAt desc"
-        ).from(1).fetch(10);
+        List<Post> olderPosts = Post.find( "order by postedAt desc").from(1).fetch(10);
         render(frontPost, olderPosts);
     }
 
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        render(post);
+    }
+
+    public static void postComment(Long postId, @Required String author, @Required String content) {
+        Post post = Post.findById(postId);
+        if (validation.hasErrors()) {
+            render("Application/show.html", post);
+        }
+        post.addComment(author, content);
+        flash.success("Thanks for posting %s", author);
+        show(postId);
+    }
 }
